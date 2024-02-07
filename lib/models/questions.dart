4b6aa33dfdc7fa4ff6a77f1cabec5f31893478af
DMC-07 <cream0948219484@gmail.com>
2024-02-07 11:49:27 +0700

    first commit

---
+++ lib/models/questions.dart
@@ -0,0 +1,95 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: '1. What is the traditional color associated with Christmas?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Green',
+      'b) Red',
+      'c) Blue',
+      'd) Yellow',
+    ],
+  ),
+  Question(
+    question: '2. What is the name of the famous reindeer with a red nose?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Dasher',
+      'b) Vixen',
+      'c) Prancer',
+      'd) Rudolph',
+    ],
+  ),
+  Question(
+    question: '3. Which country is credited with starting the tradition of putting up a Christmas tree?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) United States',
+      'b) Germany',
+      'c) England',
+      'd) France',
+    ],
+  ),
+  Question(
+    question: '4. What is the traditional Christmas meal in the United States?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Turkey',
+      'b) Ham',
+      'c) Roast beef',
+      'd) Chicken',
+    ],
+  ),
+  Question(
+    question: '5. What is the date of Christmas Day?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) December 24th',
+      'b) December 26th',
+      'c) December 25th',
+      'd) December 31st',
+    ],
+  ),
+  Question(
+    question: '6. How many days are there in the Twelve Days of Christmas?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) 6',
+      'b) 8',
+      'c) 10',
+      'd) 12',
+    ],
+  ),
+  Question(
+    question: '7. Which song is commonly sung at the end of a Christmas party or gathering?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) We Wish You a Merry Christmas',
+      'b) Jingle Bells',
+      'c) Silent Night',
+      'd) Deck the Halls',
+    ],
+  ),
+  Question(
+    question: '8. What is the name of the character who tries to stop Christmas from coming in Dr. Seuss  book “How the Grinch Stole Christmas?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Scrooge',
+      'b) Grinch',
+      'c) Santa Claus',
+      'd) Frosty',
+    ],
+  ),
+  Question(
+    question:
+        '9. What is the name of the famous painting by Leonardo da Vinci that depicts a woman?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Tulipt',
+      'b) Orchid',
+      'c) Rose',
+      'd) Poinsettia',
+    ],
+  ),
+];
